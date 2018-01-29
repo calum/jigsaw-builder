5967ab6bd3554953bcaa238c7c4611c23997a546
calum <calumforster@live.co.uk>
2018-01-29 18:42:53 +0000

    adding test for build

--- test/build_test.js
+++ test/build_test.js
@@ -1,13 +1,46 @@
 var jigsawBuilder = require('../')
-
 var assert = require('assert')
+var path = require('path')
+var fs = require('fs')
+
+/**
+* Function to test
+**/
+var build = jigsawBuilder.build
 
 describe('Jigsaw-Builder tests', function() {
-  xit('should output the correct number of jigsaw pieces', function() {
 
+  var src = path.join(__dirname, 'test_data/example.png')
+  var dest = path.join(__dirname, 'test_data/results/')
+
+  it('should output the correct number of jigsaw pieces', function(done) {
+    build(5, src, dest, function(err) {
+      if (err) done(err)
+
+      fs.readdir(dest, function(err, files) {
+        if (err) done(err)
+
+        console.log(files)
+
+        if (files.length == 26) done()
+        else done(new Error('Incorrect number of files found: '+files.length))
+
+      })
+    })
   })
 
-  xit('should output a properties.json file', function() {
+  xit('should output a properties.json file', function(done) {
+    build(5, src, dest, function(err) {
+      if (err) done(err)
 
+      fs.readdir(dest, function(err, files) {
+        if (err) done(err)
+
+        console.log(files)
+
+        if (files.indexOf('properties.json') > -1) done()
+        else done(new Error('Could not find properties.json file'))
+      })
+    })
   })
 })
